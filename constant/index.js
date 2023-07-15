fb330d2de0fd8c626246c7962ac43523cd4b075d
bahop <bahop2000@gmail.com>
2023-07-15 11:34:32 +0700

    footer

---
+++ constant/index.js
@@ -0,0 +1,41 @@
+
+export const introduceFooter = [
+  {
+    year: 'Botak Sign Pte Ltd was established in 1991.',
+    descripton:
+      'With the motto of "DESTROY TO BUILD", we have been involved in many different printing industries. From vinyl cutting to large format to digital printing, we never stop trying new things and always invest in new technologies to provide our customers with fun and high-quality products at reasonable prices.😊'
+  }
+];
+
+export const footerLinks = [
+  {
+    title: 'Explore',
+    links: [
+      { title: 'Print Products', url: '/' },
+      { title: 'Display Products', url: '/' },
+      { title: 'About Us', url: '/' },
+      { title: 'Careers', url: '/' }
+    ]
+  },
+  {
+    title: 'Need help?',
+    links: [
+      { title: 'FAQ', url: '/' },
+      { title: 'Corporate Partners', url: '/' },
+      { title: 'Terms & Conditions', url: '/' },
+      { title: 'Privacy Policy', url: '/' }
+    ]
+  },
+  {
+    title: 'Get in touch',
+    links: [
+      { title: '+(65) 9190 7688/ +(65) 6286 2298', url: '/', icons: '' },
+      {
+        title: '+(65) 9731 6551 Highland Centre #01-3422 Yio Chu Kang Road',
+        url: '/',
+        icons: ''
+      },
+      { title: 'Singapore 545535', url: '/', icons: '' }
+    ]
+  }
+];
