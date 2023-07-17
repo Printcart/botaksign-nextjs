910564f6c3079deadd4f255f615b53aa66e3279d
DaoQuangThiet <Thietdq1711@gmail.com>
2023-07-17 17:07:32 +0700

    update

--- src/app/component/Header/HeaderMiddle/Logo.js
+++ src/app/component/Header/HeaderMiddle/Logo.js
@@ -1,21 +1,20 @@
+import { headerData } from 'botak/app/constant';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 import { Col } from 'react-bootstrap';
-import LogoBotak from '../../../../../public/Botak-Logo_v3.png';
 
-
-const logo = {
-    src: `${LogoBotak?.src}`,
-    alt: 'Botak Logo'
-  };
 const Logo = () => {
   return (
     <Col className="logoheader">
       <div className="logowrapper">
         <div className="mainlogo">
           <Link href="/">
-            <Image src={logo?.src} width={230} height={80} alt={logo?.alt} />
+            <Image
+              src={headerData?.logo?.src}
+              width={230}
+              height={80}
+              alt={headerData?.logo?.title}
+            />
           </Link>
         </div>
       </div>
---
+++ src/app/constant.js
@@ -0,0 +1,11 @@
+import LogoBotak from '../../public/Botak-Logo_v3.png';
+export const headerData = {
+  logo: {
+    src: LogoBotak?.src,
+    title: 'Logo Botak'
+  },
+  icon: {
+    content: '',
+    title: 'Cart Icon'
+  }
+};
