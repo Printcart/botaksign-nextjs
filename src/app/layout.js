666b9b79b0cc5b2a1dbeedd66acb525421b7a30d
bahop <bahop2000@gmail.com>
2023-07-18 14:51:17 +0700

    update icon footer v4

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,9 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer/page';
-import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
 config.autoAddCss = false;
-
-library.add(fas);
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,9 +16,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <div>
-        <FontAwesomeIcon icon={['fa', 'coffee']} />
-      </div>
       <body className={inter.className}>
         {children}
         <Footer />
