ccd993384d5f9ce3f70cf40f465d455f90a58224
bahop <bahop2000@gmail.com>
2023-07-18 14:49:33 +0700

    update icon footer v3

--- src/app/components/Footer/IconFooter.jsx
+++ src/app/components/Footer/IconFooter.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fas);
+import styles from './footer.module.css';
 
 const IconFooter = ({ attr, lib }) => {
-  return <i className={`${lib} ${lib}-${attr}`}></i>;
+  return (
+    <FontAwesomeIcon icon={[`${lib}`, `${attr}`]} className={styles.iconFooter} />
+  );
 };
 
 export default IconFooter;
