b09c1cc77ab7b2258ba777085af1452c74421360
bahop <bahop2000@gmail.com>
2023-07-31 11:18:06 +0700

    update remove console

--- src/app/product-category/posters-cards/[id]/[product].jsx
+++ src/app/product-category/posters-cards/[id]/[product].jsx
@@ -3,7 +3,6 @@ import { Card, Col, Row } from 'react-bootstrap';
 import styles from './page.module.css';
 
 export const Products = (props) => {
-  console.log(props);
   const { data } = props;
 
   return (
