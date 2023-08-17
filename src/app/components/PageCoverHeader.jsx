7a7dda52bd76515aa243132074c5e9274162d1d2
bahop <bahop2000@gmail.com>
2023-08-17 18:14:42 +0700

    colour chart

---
+++ src/app/components/PageCoverHeader.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './PageCoverHeader.module.css';
+
+const PageCoverHeader = (props) => {
+  const { title, link, titlePage } = props;
+  return (
+    <>
+      <div className={styles.contactHeader}>
+        <div className={styles.contactWrap}>
+          <h1>{title}</h1>
+          <span className={styles.contactLink}>
+            <Link href="/">{link}</Link>
+            <span>/</span>
+            <span className={styles.breadcrumbLast}>{titlePage}</span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PageCoverHeader;
