65ceb2d454a9c96c7e870f7c32f6456db822b41c
bahop <bahop2000@gmail.com>
2023-08-18 09:44:43 +0700

    update

--- src/app/colour-chart/page.jsx
+++ src/app/colour-chart/page.jsx
@@ -14,6 +14,7 @@ const PrintingColour = () => {
       </div>
       <Link href="https://botaksign.com/wp-content/uploads/2020/05/Printing-Colour-Chart-A4.pdf">
         <Image
+          className={styles.image}
           width={400}
           height={400}
           src="https://botaksign.com/wp-content/uploads/2020/05/Printing-Colour-Chart-1.jpg "
@@ -26,54 +27,60 @@ const PrintingColour = () => {
 
 const StickerColour = () => {
   return (
-    <div className={styles.nav_content}>
-      <div className={styles.contentTitle}>
-        <div>
-          <p className={styles.contentTitle}>Sticker Colour Chart</p>
-        </div>
-        <p className={styles.contentSubtitle}>Vinyl Sticker</p>
-      </div>
-      <div className={styles.conditionsBlock}>
-        <Link href="https://botaksign.com/wp-content/uploads/2020/05/CMYK-for-vinyl-sticker-colour-1st-June-20-view.pdf">
-          <Image
-            width={400}
-            height={400}
-            src="https://botaksign.com/wp-content/uploads/2020/05/CMYK-for-vinyl-sticker-colour-1st-June-20-view-2-scaled.jpg"
-            alt="image"
-          />
-        </Link>
-      </div>
-      <div id="nav_COWOB" className={styles.conditionsBlock}>
-        <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/2020/08/vinyl-sticker-colour-chart-colour.pdf">
-          <Image
-            width={400}
-            height={400}
-            src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/2020/08/vinyl-sticker-colour-chart-colour-2.jpg"
-            alt="image"
-          />
-        </Link>
-      </div>
+    <div id="nav_CO" className={styles.nav_content}>
       <div id="nav_COWOB">
-        <p>Special Effects Sticker</p>
+        <div className={styles.contentTitle}>
+          <div>
+            <p className={styles.contentTitle}>Sticker Colour Chart</p>
+          </div>
+          <p className={styles.contentSubtitle}>Vinyl Sticker</p>
+        </div>
         <div className={styles.conditionsBlock}>
-          <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_1-2.jpg">
+          <Link href="https://botaksign.com/wp-content/uploads/2020/05/CMYK-for-vinyl-sticker-colour-1st-June-20-view.pdf">
             <Image
+              className={styles.image}
               width={400}
               height={400}
-              src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_1-2.jpg"
+              src="https://botaksign.com/wp-content/uploads/2020/05/CMYK-for-vinyl-sticker-colour-1st-June-20-view-2-scaled.jpg"
               alt="image"
             />
           </Link>
         </div>
         <div className={styles.conditionsBlock}>
-          <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_2-2.jpg">
+          <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/2020/08/vinyl-sticker-colour-chart-colour.pdf">
             <Image
+              className={styles.image}
               width={400}
               height={400}
-              src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_2-2.jpg"
+              src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/2020/08/vinyl-sticker-colour-chart-colour-2.jpg"
               alt="image"
             />
           </Link>
+        </div>
+        <div id="nav_COWB">
+          <p className={styles.contentSubtitle}>Special Effects Sticker</p>
+          <div className={styles.conditionsBlock}>
+            <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_1-2.jpg">
+              <Image
+                className={styles.image}
+                width={400}
+                height={400}
+                src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_1-2.jpg"
+                alt="image"
+              />
+            </Link>
+          </div>
+          <div className={styles.conditionsBlock}>
+            <Link href="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_2-2.jpg">
+              <Image
+                className={styles.image}
+                width={400}
+                height={400}
+                src="https://botaksign-library.s3.ap-southeast-1.amazonaws.com/material spec and photos/colour chart/specialsticker_stickerchart_2-2.jpg"
+                alt="image"
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
@@ -117,7 +124,7 @@ const ColourChart = () => {
     <div>
       <PageCoverHeader title="Colour Charts" link="Home" titlePage="Colour Charts" />
       <Container>
-        <Row>
+        <Row className={styles.content}>
           <Col lg={3} className={styles.navigationBlock}>
             <NavigationBlock />
           </Col>
