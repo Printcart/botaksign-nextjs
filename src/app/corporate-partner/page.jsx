f0c393f2235b193971476db2898cfd1fee9b6e42
bahop <bahop2000@gmail.com>
2023-08-18 09:55:33 +0700

    Corporate Partner

---
+++ src/app/corporate-partner/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageCoverHeader from '../components/PageCoverHeader';
+
+const CorporatePartner = () => {
+  return (
+    <div>
+      <PageCoverHeader
+        title="Corporate Partner"
+        link="Home"
+        titlePage="Corporate Partner"
+      />
+    </div>
+  );
+};
+
+export default CorporatePartner;
