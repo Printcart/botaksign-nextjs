0c43842f4779bec52b7ac3ce715ff0ce4e95e7ee
bahop <bahop2000@gmail.com>
2023-08-23 10:57:22 +0700

    update

---
+++ src/app/privacy-policy/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const PrivacyPolicy = (props) => {
+  const { data } = props;
+  console.log(data);
+  return <Container><div>
+    </div></Container>;
+};
+
+export default PrivacyPolicy;
