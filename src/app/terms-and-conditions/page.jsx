9d6ff1a2be4082393b214d0bdac8b7af95efd883
bahop <bahop2000@gmail.com>
2023-08-24 14:18:48 +0700

    update

--- src/app/terms-and-conditions/page.jsx
+++ src/app/terms-and-conditions/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TermsAndConditions from './TermsAndConditions';
-import { fetchTermsAndConditions } from 'botak/api/termsAndConditionsPgae';
+import { fetchTermsAndConditions } from 'botak/api/pages';
 
 const TermsAndConditionsServer = async () => {
   const data = await fetchTermsAndConditions();
