5bab2bf12885cf024475445db72b42d9f2ac8643
bahop <bahop2000@gmail.com>
2023-09-08 10:30:51 +0700

    update

--- src/app/[year]/[month]/page.jsx
+++ src/app/[year]/[month]/page.jsx
@@ -11,7 +11,6 @@ const Page = async ({ params }) => {
   return (
     <Archives
       dataCategories={dataCategories}
-      params={params}
       dataDate={dataDate}
       year={year}
       month={month}
